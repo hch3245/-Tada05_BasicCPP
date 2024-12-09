a9166ac32c6cca9fc18d5c1283eaa70bb7099b93
HallofFame727 <hch3245@naver.com>
2024-12-09 21:51:48 +0900

    1209 수업
    Delegate를 통한 콜백을 배움
    
    Multicast를 통한 다중 콜백을 배움

---
+++ Source/BasicCPP/Delegate/CMulticast.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CMulticast.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void ACMulticast::BeginPlay() {
+	Super::BeginPlay();
+
+	OnActorBeginOverlap.AddDynamic(this, &ACMulticast::OnBeginOverlap);
+
+}
+
+void ACMulticast::OnBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	int32 RandomIndex = UKismetMathLibrary::RandomIntegerInRange(0, 2);
+	FLinearColor RandomColor = FLinearColor::MakeRandomColor();
+
+	OnMulticastOverlap.Broadcast(RandomIndex, RandomColor);
+}
