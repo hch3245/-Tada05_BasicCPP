df7cc7fdd063739ee613e4a2413c3777939a681d
HallofFame727 <hch3245@naver.com>
2024-12-10 21:52:50 +0900

    1210 수업 내용
    오늘 배운 것
    함수에 대해 블루프린트에서 가져다 쓰거나 override 하는 방법 배움
    가져다 쓰기 UFUNCTION(BlueprintCallable)
    무조건 덮어쓰기 C++은 선언만 구현 X UFUNCTION(BlueprintImplementableEvent)
    
    블프에서 Override, C++ 꺼 쓰고 싶으면 부모 호출
    UFUNCTION(BlueprintNativeEvent)

--- Source/BasicCPP/Delegate/CSpotLight.cpp
+++ Source/BasicCPP/Delegate/CSpotLight.cpp
@@ -2,6 +2,8 @@
 
 #include "CSpotLight.h"
 #include "Components/SpotLightComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "CMulticast.h"
 
 ACSpotLight::ACSpotLight()
 {
@@ -17,7 +19,10 @@ ACSpotLight::ACSpotLight()
 		SpotLightComps[i] = CreateDefaultSubobject<USpotLightComponent>(FName(CompName));
 		SpotLightComps[i]->SetupAttachment(RootComp);
 
-
+		SpotLightComps[i]->SetRelativeRotation(FRotator(-90, 0, 0));
+		SpotLightComps[i]->SetRelativeLocation(FVector(0, i * 150, 0));
+		SpotLightComps[i]->SetIntensity(1e+5f);
+		SpotLightComps[i]->SetOuterConeAngle(25.f);
 	}
 }
 
@@ -25,11 +30,27 @@ void ACSpotLight::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	TArray<AActor*> Actors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACMulticast::StaticClass(), Actors);
+
+	if (Actors.Num() > 0) {
+		ACMulticast* Trigger = Cast<ACMulticast>(Actors[0]);
+
+		if (Trigger) {
+			Trigger->OnMulticastOverlap.AddUFunction(this, "OnLight");
+		}
+	}
+
 }
 
 void ACSpotLight::OnLight(int32 InIndex, FLinearColor InColor)
 {
+	for (int32 i = 0; i < 3; i++) {
+		SpotLightComps[i]->SetLightColor(FLinearColor::White);
 
+	}
+
+	SpotLightComps[InIndex]->SetLightColor(InColor);
 }
 
 
